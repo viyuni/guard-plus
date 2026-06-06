03fcd790a6afdd929840970ecc958f654682ed28
byc <bycrxemail@gmail.com>
2026-06-06 16:40:51 +0800

    ci(release): add changelogen release workflow

--- server/src/utils/api.ts
+++
@@ -1,8 +0,0 @@
-// import type { AdminApi } from '~/plugins/api';
-
-// export const api = new Proxy({} as AdminApi, {
-//   get(_target, prop, receiver) {
-//     const { $api } = useNuxtApp();
-//     return Reflect.get($api, prop, receiver);
-//   },
-// });
